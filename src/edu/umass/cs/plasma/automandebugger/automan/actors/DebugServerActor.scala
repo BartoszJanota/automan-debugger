611d7ff8eea92b8f2866b227896b00d5b5cefd26
BartoszJanota <bartosz.janota@gmail.com>
2015-06-25 13:06:56 +0200

    components fixed (all related to scala 2.11.4)

--- src/edu/umass/cs/plasma/automandebugger/automan/actors/DebugServerActor.scala
+++ src/edu/umass/cs/plasma/automandebugger/automan/actors/DebugServerActor.scala
@@ -2,16 +2,16 @@ package edu.umass.cs.plasma.automandebugger.automan.actors
 
 import akka.actor.{Actor, ActorRefFactory}
 import edu.umass.cs.automan.core.AutomanAdapter
+import edu.umass.cs.plasma.automandebugger.models.{TaskSnapshotResponse, Tasks}
+
 //import org.slf4j.Logger
-import spray.json._
-import spray.routing._
 import spray.routing.RejectionHandler.Default
-import spray.util.LoggingContext
+import spray.routing._
 
 /**
  * Created by bj on 25.06.15.
  */
-class DebugServerActor(adapter: AutomanAdapter) extends Actor with HttpService with TaskSnapshotJsonProtocol {
+class DebugServerActor(adapter: AutomanAdapter) extends Actor with HttpService {
   override implicit def actorRefFactory: ActorRefFactory = context
 
   println("AutoManDebugger Server has started....")
@@ -21,13 +21,11 @@ class DebugServerActor(adapter: AutomanAdapter) extends Actor with HttpService w
 
   override def receive: Receive = runRoute(routes)
 
- // Logger logger = new LoggingContext {}
-
   val routes: Route = path("state") {
     pathEndOrSingleSlash {
       get {
         complete {
-          getAdapterState().toJson.toString()
+          getAdapterState().toString
         }
       }
     }
--- src/edu/umass/cs/plasma/automandebugger/models/TaskSnapshotResponse.scala
+++ src/edu/umass/cs/plasma/automandebugger/models/TaskSnapshotResponse.scala
@@ -1,16 +1,16 @@
-package edu.umass.cs.plasma.automandebugger.automan.actors
+package edu.umass.cs.plasma.automandebugger.models
 
-import spray.json.DefaultJsonProtocol
+//import spray.json.DefaultJsonProtocol
 
 /**
  * Created by bj on 25.06.15.
  */
-trait TaskSnapshotJsonProtocol extends DefaultJsonProtocol{
+/*trait TaskSnapshotJsonProtocol extends DefaultJsonProtocol{
   //def apply(t: TaskSnapshot[_]): TaskSnapshotResponse = new TaskSnapshotResponse(title = t.title, text = t.text)
 
   implicit val taskSnapshotResponseFormat = jsonFormat2(TaskSnapshotResponse.apply)
   implicit val tasks = jsonFormat1(Tasks.apply)
-}
+}*/
 
 case class TaskSnapshotResponse(title: String, text: String) {}
 
