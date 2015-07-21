f08e31fb3ec429ae304511df6edc400a0a3bc5bd
BartoszJanota <bartosz.janota@gmail.com>
2015-07-21 20:01:27 +0200

    adedd JavaFX thread handler and AID refresh button

--- src/edu/umass/cs/plasma/automandebugger/idea/browser/IdeaPluginToolBrowserWindow.scala
+++ src/edu/umass/cs/plasma/automandebugger/idea/browser/IdeaPluginToolBrowserWindow.scala
@@ -1,5 +1,6 @@
 package edu.umass.cs.plasma.automandebugger.idea.browser
 
+import java.awt.event.{ActionEvent, ActionListener}
 import java.awt.{Dimension, BorderLayout}
 import java.io._
 import java.util.Scanner
@@ -26,54 +27,67 @@ class IdeaPluginToolBrowserWindow extends ToolWindowFactory{
 
     val mainPanel = new JPanel(new BorderLayout())
     mainPanel.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3))
-    mainPanel.setPreferredSize(new Dimension(150, 450))
+    //mainPanel.setPreferredSize(new Dimension(300, 450))
 
-    val browserPanel = new JPanel(new BorderLayout())
+    val jfxPanel: JFXPanel = new JFXPanel()
 
-    val jfxPanel = new JFXPanel()
+    Platform.setImplicitExit(false);
 
     Platform.runLater(new Runnable {
       override def run(): Unit = {
-
-        val stage = new Stage()
-        stage.setTitle("StageJava FX")
-        stage.setResizable(true)
-
-        val root = new Group()
-        val scene = new Scene(root, 100, 450)
-        stage.setScene(scene)
-        //scene.getStylesheets.
-        //val css = scala.io.Source.fromInputStream(getClass.getResourceAsStream("/bootstrap.css")).mkString
-        //scene.getStylesheets.add(css)
-        val webView = new WebView()
-        //webView.getStylesheets.add(css)
-
-        val webEngine = webView.getEngine
-        val text = scala.io.Source.fromInputStream(getClass.getResourceAsStream("/index.html")).mkString
-        val debuggerJsFastopt = scala.io.Source.fromInputStream(getClass.getResourceAsStream("/automan-debugger-frontend-fastopt.js")).mkString
-        val debuggerJsLauncher = scala.io.Source.fromInputStream(getClass.getResourceAsStream("/automan-debugger-frontend-launcher.js")).mkString
-
-        webEngine.setJavaScriptEnabled(true)
-        webEngine.loadContent(text)
-        val children = root.getChildren
-        children.add(webView)
-
-        webEngine.getLoadWorker.stateProperty.addListener(new ChangeListener[State]() {
-          override def changed(observableValue: ObservableValue[_ <: State], t: State, t1: State): Unit = {
-            if (t1 == Worker.State.SUCCEEDED) {
-              webEngine.executeScript(debuggerJsFastopt)
-              webEngine.executeScript(debuggerJsLauncher)
-            }
-
-          }
-        })
-        jfxPanel.setScene(scene)
+        loadPage(jfxPanel)
+        mainPanel.add(jfxPanel, BorderLayout.NORTH)
       }
     })
 
-    browserPanel.add(jfxPanel, BorderLayout.CENTER)
-    mainPanel.add(browserPanel, BorderLayout.CENTER)
+    val refreshButton: JButton = new JButton("Refresh AID")
+    refreshButton.addActionListener(new ActionListener {
+      override def actionPerformed(e: ActionEvent): Unit = {
+        println("refreshing now!")
+        Platform.runLater(new Runnable {
+          override def run(): Unit = {
+            mainPanel.remove(jfxPanel)
+            loadPage(jfxPanel)
+            mainPanel.add(jfxPanel, BorderLayout.NORTH)
+          }
+        })
+      }
+    })
 
+
+    mainPanel.add(refreshButton, BorderLayout.SOUTH)
     component.add(mainPanel, BorderLayout.CENTER)
+  }
+
+  def loadPage(jfxPanel: JFXPanel): Unit = {
+    val stage = new Stage()
+    stage.setTitle("StageJava FX")
+    stage.setResizable(true)
+
+    val root = new Group()
+    val scene = new Scene(root, 500, 500)
+    stage.setScene(scene)
+    val webView = new WebView()
+
+    val webEngine = webView.getEngine
+    val text = scala.io.Source.fromInputStream(getClass.getResourceAsStream("/index.html")).mkString
+    val debuggerJsFastopt = scala.io.Source.fromInputStream(getClass.getResourceAsStream("/automan-debugger-frontend-fastopt.js")).mkString
+    val debuggerJsLauncher = scala.io.Source.fromInputStream(getClass.getResourceAsStream("/automan-debugger-frontend-launcher.js")).mkString
+
+    webEngine.setJavaScriptEnabled(true)
+    webEngine.loadContent(text)
+    val children = root.getChildren
+    children.add(webView)
+
+    webEngine.getLoadWorker.stateProperty.addListener(new ChangeListener[State]() {
+      override def changed(observableValue: ObservableValue[_ <: State], t: State, t1: State): Unit = {
+        if (t1 == Worker.State.SUCCEEDED) {
+          webEngine.executeScript(debuggerJsFastopt)
+          webEngine.executeScript(debuggerJsLauncher)
+        }
+
+      }
+    })
+    jfxPanel.setScene(scene)
   }
 }
